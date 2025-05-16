69367c09da750589be9b1bb1ca7f3d4addb8ed36
Vadim91199 <vadimpapavets@gmail.com>
2025-05-16 17:22:15 +0300

    Create oaiplab6.1.cpp
    hh

---
+++ oaiplab6.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "RU");
+    string input;
+    cout << "Введите строку: ";
+    getline(cin, input);
+
+    int length = input.length();
+    cout << "Длина строки: " << length << endl;
+                                                            
+    if (length == 10) {
+        string result;
+        for (char ch : input) {
+            if (!(ch >= 'A' && ch <= 'Z')) {
+                result += ch;
+            }
+        }
+        cout << "Результат: " << result << endl;
+    }
+    else {
+        cout << "Строка не требует изменений." << endl;
+    }
+
+    return 0;
+}
